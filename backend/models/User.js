327e1b54699da256738a24d48b60aba85bf0cc0e
Saksham Kanojia <sakshamkanojia2002@gmail.com>
2025-02-06 17:27:06 +0530

    c3

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
-  phone: String,
-  textEditorData: String
+  phone: String
 });
 
 export default mongoose.model("User", UserSchema);
